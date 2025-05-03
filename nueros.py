e2f74f8499e30fe13b4a0a3671aa88a40006e7c1
YAYO5478 <edisonyahirgarciagonzalez1@gmail.com>
2025-05-03 16:55:52 -0600

    para checar que royo salio mal

--- nueros.py
+++ nueros.py
@@ -1,29 +1,29 @@
-def calcular(n1, operacion, n2):
+def calcular(n1,operacion,n2):
     if operacion == "+":
         return n1 + n2
-    elif operacion == "-":
-        return n1 - n2
+    elif operacion =="-":
+        return n1 -n2
     elif operacion == "*":
         return n1 * n2
     elif operacion == "/":
         if n2 != 0:
             return n1 / n2
         else:
-            print("Error: división por cero.")
+            print("Error: division por cero no valida")
             return n1
-    else:
-        print("Operación no válida.")
+    else: 
+        print("Operacion no valida")
         return n1
-
-# Iniciar con un número
-n = float(input("Ingrese un número: "))
-
-while True:
-    operacion = input("Ingrese una operación (+, -, *, /): ")
-    try:
-        n2 = float(input("Ingrese otro número: "))
-        resultado = calcular(n, operacion, n2)
-        print("Resultado:", resultado)
-        n = resultado  # Actualizamos n con el resultado
-    except ValueError:
-        print("Entrada no válida. Por favor, ingrese un número.")
+    
+    # Iniciar con un numero
+    n= float(input("Ingrese un numero;"))
+    
+    while True:
+        operacion = input("Ingrese una operacion (+,-,*,/):")
+        try:
+            n2 = float(input("Ingrese otro numero:"))
+            resultado = calcular (n ,operacion,n2)
+            print("Resultado:", resultado)
+            n= resultado # Actualizamos n con el resultado
+        except ValueError:
+            print("Entrada no valida. Por favor,ingrese una entrada valida")
